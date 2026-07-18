b3342df1a347a14918d570b71eeeb7a23bb9dfd5
SafaMahmoud013 <138758482+SafaMahmoud013@users.noreply.github.com>
2026-07-18 03:32:16 +0300

    Add overdue task highlighting using computed field

--- todo_list/models/todo_task.py
+++ todo_list/models/todo_task.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import date
 
 
 class TodoTask(models.Model):
@@ -18,10 +19,26 @@ class TodoTask(models.Model):
         default='new'
     )
     priority = fields.Selection([
-        ('0', 'Low'),
-        ('1', 'Medium'),
-        ('2', 'High'),
-    ], string="Priority", default='1')
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ], string="Priority", default="medium")
+    is_overdue = fields.Boolean(
+        string="Overdue",
+        compute="_compute_is_overdue"
+    )
+
+    @api.depends("due_date", "status")
+    def _compute_is_overdue(self):
+        today = fields.date.today()
+
+        for record in self:
+            record.is_overdue = (
+                    record.due_date
+                    and record.due_date < today
+                    and record.status != "completed"
+            )
+
 
 class TodoEmployee(models.Model):
     _name = "todo.employee"
